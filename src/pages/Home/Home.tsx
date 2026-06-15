35ad7ee1d94f571a9df78486ba696f363023a5d7
akmaljonyusupov <infoakmaljon.uz@gmail.com>
2026-06-16 04:12:12 +0500

    hero

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,41 @@
 import HeroSlider from "../../components/HeroSlider/HeroSlider";
+import tintteraImg from "../../assets/about-tintera.png";
+import "./Home.scss";
 
 function Home() {
   return (
     <>
       <HeroSlider />
 
-      <section>
-        Популярные товары
-      </section>
+      <section className="about-tintera">
+        <div className="about-container">
+          <div className="about-left">
+            <h2>О Тинтере</h2>
 
-      <section>
-        Наши преимущества
-      </section>
+            <p>
+              Центр Tintera Decor создан 1 мая 2023 года в Шайхонтохурском
+              районе города Ташкента. В нашем центре декора Tintera для вас,
+              дорогие наши клиенты, представлен широкий ассортимент
+              качественных декоративных красок и штукатурок.
+            </p>
 
-      <section>
-        Галерея
+            <p>
+              Мы надеемся, что Tintera станет не только магазином красок, но и
+              творческим домом для начинающих мастеров и профессионалов.
+              Продукция соответствует европейским стандартам качества и
+              сохраняет насыщенность цвета на долгие годы.
+            </p>
+
+            <p>
+              Добро пожаловать в мир дизайна, вдохновения и современных
+              декоративных решений вместе с Tintera Decor.
+            </p>
+          </div>
+
+          <div className="about-right">
+            <img src={tintteraImg} alt="Tintera Decor" />
+          </div>
+        </div>
       </section>
     </>
   );
