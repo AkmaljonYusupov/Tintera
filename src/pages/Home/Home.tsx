912d3aff7c3be6a98f58fb0132aaa95b4e4eb0ea
akmaljonyusupov <infoakmaljon.uz@gmail.com>
2026-06-20 23:34:08 +0500

    About

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import HeroSlider from "../../components/HeroSlider/HeroSlider";
-import AboutTintera from "../../components/AboutTintera/About";
+import AboutTintera from "../../components/AboutTintera/AboutTintera";
 import StatsVideo from "../../components/StatsVideo/StatsVideo";
 import InstagramReels from "../../components/InstagramReels/InstagramReels";
 import Map from "../../components/Map/Map";
